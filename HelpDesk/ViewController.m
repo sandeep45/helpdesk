bf3368000a75e5a12566540e6957fc0f7f6545b3
Sandeep <sandeep@assistly.com>
2012-12-12 10:13:20 -0500

    initial commit with single view app loading desk.com helpdesk

--- HelpDesk/ViewController.m
+++ HelpDesk/ViewController.m
@@ -16,8 +16,20 @@
 
 - (void)viewDidLoad
 {
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *urlAddress = @"https://support.desk.com";
+    
+    //Create a URL object.
+    NSURL *url = [NSURL URLWithString:urlAddress];
+    
+    //URL Requst Object
+    NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
+    
+    //Load the request in the UIWebView.
+    [webPage loadRequest:requestObj];
 }
 
 - (void)didReceiveMemoryWarning
