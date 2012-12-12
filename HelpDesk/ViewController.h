594e111e8168efb608aad7b456d68459af1b29de
Sandeep <sandeep@assistly.com>
2012-12-12 04:04:45 -0500

    Initial Commit

---
+++ HelpDesk/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelpDesk
+//
+//  Created by Sandeep Arneja on 12/12/12.
+//  Copyright (c) 2012 Sandeep Arneja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ HelpDesk/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelpDesk
+//
+//  Created by Sandeep Arneja on 12/12/12.
+//  Copyright (c) 2012 Sandeep Arneja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
